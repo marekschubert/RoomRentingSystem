8ae7037a0e0352715059baf26eac4a941416ac40
Marek Schubert <m.schubert@op.pl>
2023-06-16 14:24:59 +0200

    Update RoomController.cs

--- backend/RoomReservationSystem/RoomReservationSystem/Controllers/RoomController.cs
+++ backend/RoomReservationSystem/RoomReservationSystem/Controllers/RoomController.cs
@@ -7,5 +7,20 @@ namespace RoomReservationSystem.Controllers
     [ApiController]
     public class RoomController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetRoomPositionData()
+        {
+            string filePath = "\\Assets\\rooms.json";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            string jsonContent = System.IO.File.ReadAllText(filePath);
+
+            return Content(jsonContent, "application/json"); // Odpowiedź zawierająca treść pliku JSON jako odpowiedni typ MIME
+
+        }
     }
 }
